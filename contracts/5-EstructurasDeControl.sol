dc195291ade05f7789c00a4752378a03bcbde027
taco72 <0xtaco7295@gmail.com>
2022-02-15 22:28:40 -0500

    added an event to EstructurasDeControl.sol (class: eventos

--- contracts/5-EstructurasDeControl.sol
+++ contracts/5-EstructurasDeControl.sol
@@ -5,12 +5,16 @@ contract EstructurasDeControl {
     uint256[] public numeros;
     string public resultado;
 
+    event NotificacionDeCondicion(bool condicion);
+
     constructor(bool condicion) {
         if (condicion) {
             resultado = "condicion true";
         } else {
             resultado = "condicion false";
         }
+
+        emit NotificacionDeCondicion(condicion);
 
         for (uint256 iterador = 0; iterador < 10; iterador++) {
             numeros.push(iterador);
