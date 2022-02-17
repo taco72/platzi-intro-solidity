407b6e8b43e961aa63be07b591a5d0e89786145a
taco72 <0xtaco7295@gmail.com>
2022-02-17 12:32:45 -0500

    installed openzeppelin contracts

--- contracts/11-Import.sol
+++ contracts/11-Import.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0 <0.9.0;
 
-import "@openzeppelin/contracts/utils/math/SafeMath.sol"; //it works on remix but not on vscode lmao
+/* if the import shows an error on vscode remember to npm install @openzeppelin/contracts */
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Import {
     function sumarNumero(uint256 numero1, uint256 numero2)
